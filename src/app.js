22d6644e64b384387829227e038f666e137691fb
birando <birger1@live.com>
2025-04-29 21:00:27 +0200

    feat(header): create and style header

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("layout");
 });
 
 app.listen(port, () => {
