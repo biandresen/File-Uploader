fc6a739c6193db694992337f6971fa643dd95528
birando <birger1@live.com>
2025-05-13 14:21:02 +0200

    fix: cookie setting samSite

--- src/app.js
+++ src/app.js
@@ -61,8 +61,9 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 30,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "production", // Use 'true' only in production (HTTPS)
       httpOnly: true,
+      sameSite: "None", // Cross-origin support
     },
   })
 );
